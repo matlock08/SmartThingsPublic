a412c2289e9b9952f5f9f63e398211c2009b1b79
Gerardo Rocha <grochanieto@gmail.com>
2016-12-20 08:06:13 -0600

    added firebase logic

---
+++ devicetypes/matlock08/aeon-hem-v1.src/aeon-hem-v1.groovy
@@ -0,0 +1,211 @@
+/**
+ *  Aeon HEM V1
+ *
+ *  Copyright 2016 Jose Castellanos
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+metadata {
+	definition (name: "Aeon HEM V1", namespace: "matlock08", author: "Jose Castellanos", oauth: true) {
+		capability "Configuration"
+		capability "Energy Meter"
+		capability "Power Meter"
+		capability "Sensor"
+
+		attribute "energyCost", "string"
+        
+        command "reset"
+
+		fingerprint deviceId: "0x2101", inClusters: "0x70,0x31,0x72,0x86,0x32,0x80,0x85,0x60"
+	}
+
+// simulator metadata
+	simulator {
+		for (int i = 0; i <= 10000; i += 1000) {
+			status "power  ${i} W": new physicalgraph.zwave.Zwave().meterV1.meterReport(
+				scaledMeterValue: i, precision: 3, meterType: 4, scale: 2, size: 4).incomingMessage()
+		}
+		for (int i = 0; i <= 100; i += 10) {
+			status "energy  ${i} ": new physicalgraph.zwave.Zwave().meterV1.meterReport(
+				scaledMeterValue: i, precision: 3, meterType: 0, scale: 0, size: 4).incomingMessage()
+		}
+	}
+
+	// tile definitions
+	tiles {
+		valueTile("power", "power") {
+			state "default", label:'${currentValue} W'
+		}
+		valueTile("energy", "energy", decoration: "flat") {
+			state "default", label:'${currentValue} Kwh'
+        }
+        valueTile("energyCost", "energyCost", decoration: "flat") {
+            state "default", label: '${currentValue}'
+		}
+		standardTile("reset", "energy", inactiveLabel: false, decoration: "flat") {
+			state "default", label:'reset', action:"reset"
+		}
+		standardTile("refresh", "power", inactiveLabel: false, decoration: "flat") {
+			state "default", label:'', action:"refresh.refresh", icon:"st.secondary.refresh"
+		}
+
+		graphTile(name: "powerGraph", attribute: "power" )
+        		
+		main (["power","energy","energyCost"])
+
+		details(["powerGraph","power","energy","energyCost", "reset","refresh", "configure"])
+ 	}
+    
+    preferences {
+        input "kWhCost", "string", title: "\$/kWh (0.16)", defaultValue: "0.16" as String
+    }
+}
+
+
+// ========================================================
+// PREFERENCES
+// ========================================================
+
+preferences {
+	input name: "graphPrecision", type: "enum", title: "Graph Precision", description: "Daily", required: true, options: graphPrecisionOptions(), defaultValue: "Daily"
+	input name: "graphType", type: "enum", title: "Graph Type", description: "line", required: false, options: graphTypeOptions(), defaultValue: "line"
+}
+
+// ========================================================
+// MAPPINGS
+// ========================================================
+
+mappings {
+	path("/graph/:attribute") {
+		action:
+		[
+			GET: "renderGraph"
+		]
+	}
+}
+
+def renderGraph() {
+	log.debug "renderGraph ${params.attribute}"
+	def data = fetchGraphData(params.attribute)
+	def averageData = data*.average
+
+	def xValues = data*.unixTime
+
+	def yValues = [
+		Total: [color: "#49a201", data: averageData, type: "line"]
+	]
+    
+    log.debug "yValues ${yValues}"
+    log.debug "xValues ${xValues}"
+
+	renderGraph(attribute: params.attribute, xValues: xValues, yValues: yValues, focus: "Total", label: "Watts")
+}
+
+// ========================================================
+// Z-WAVE
+// ========================================================
+
+
+def parse(String description) {
+	def result = null
+	def cmd = zwave.parse(description, [0x31: 1, 0x32: 1, 0x60: 3])
+	if (cmd) {
+		result = createEvent(zwaveEvent(cmd))
+	}
+	log.debug "Parse returned ${result?.name} - ${result?.descriptionText}"
+
+	storeGraphData(result.name, result.value)
+    
+    
+	def date = new Date()
+	def sdf = new java.text.SimpleDateFormat("MM/dd/yyyy HH:mm:ss")
+	    
+    def params = [
+		uri: "https://homemonitoring-73711.firebaseio.com/hem.json?auth=9GxXMikppcOtKiTAQRpiQFI0mXfodq5zMHDjekFL",
+		body: [
+			"event": result?.name ,
+			"value": result?.value ,
+			"unit": result?.unit,
+            "date": sdf.format(date)
+		]
+	]
+    
+    try {
+		httpPostJson(params) { resp ->
+			resp.headers.each {
+				//log.debug "${it.name} : ${it.value}"
+			}
+			log.debug "DEBUG (POST FIREBASE): response contentType: ${resp.contentType}"
+		}
+	} catch (e) {
+		log.debug "something went wrong: $e"
+	}
+
+	return result
+}
+
+def zwaveEvent(physicalgraph.zwave.commands.meterv1.MeterReport cmd) {
+
+    def dispValue
+    def newValue
+    
+	if (cmd.scale == 0) {	
+        newValue = cmd.scaledMeterValue
+        
+        dispValue = String.format("%5.2f",newValue)
+        sendEvent(name: "energy", value: dispValue as String, unit: "kWh")
+        state.energyValue = newValue
+        BigDecimal costDecimal = newValue * ( kWhCost as BigDecimal)
+        def costDisplay = String.format("%5.2f",costDecimal)
+        sendEvent(name: "energyCost", value: "\$${costDisplay}", unit: "")
+        [name: "energy", value: cmd.scaledMeterValue, unit: "kWh"]
+    }
+    else if (cmd.scale == 1) {
+        [name: "energy", value: cmd.scaledMeterValue, unit: "kVAh"]
+    }
+    else {
+        [name: "power", value: Math.round(cmd.scaledMeterValue), unit: "W"]
+    }
+}
+
+def zwaveEvent(physicalgraph.zwave.Command cmd) {
+	// Handles all Z-Wave commands we aren't interested in
+	[:]
+}
+
+def refresh() {
+	delayBetween([
+		zwave.meterV2.meterGet(scale: 0).format(),
+		zwave.meterV2.meterGet(scale: 2).format()
+	])
+}
+
+def reset() {
+
+	sendEvent(name: "energyCost", value: "Cost\n--", unit: "")
+    return [
+            zwave.meterV2.meterReset().format(),
+            zwave.meterV2.meterGet(scale: 0).format()
+   ]
+}
+
+def configure() {
+	def cmd = delayBetween([
+		zwave.configurationV1.configurationSet(parameterNumber: 101, size: 4, scaledConfigurationValue: 4).format(),   // combined power in watts
+		zwave.configurationV1.configurationSet(parameterNumber: 111, size: 4, scaledConfigurationValue: 20).format(), // every 20s
+		zwave.configurationV1.configurationSet(parameterNumber: 102, size: 4, scaledConfigurationValue: 8).format(),   // combined energy in kWh
+		zwave.configurationV1.configurationSet(parameterNumber: 112, size: 4, scaledConfigurationValue: 20).format(), // every 20s
+		zwave.configurationV1.configurationSet(parameterNumber: 103, size: 4, scaledConfigurationValue: 0).format(),    // no third report
+		zwave.configurationV1.configurationSet(parameterNumber: 113, size: 4, scaledConfigurationValue: 20).format() // every 20s
+	])
+	log.debug cmd
+	cmd
+}
